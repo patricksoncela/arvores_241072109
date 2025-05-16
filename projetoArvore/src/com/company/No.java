6f9309c7e5fc7eaaf53c66265e68585876c1da09
Patrick <133254356+patricksoncela@users.noreply.github.com>
2025-05-15 21:58:15 -0300

    Commit inicial [Classe Main, Arvore e No]

---
+++ projetoArvore/src/com/company/No.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class No {
+    String valor;
+    No esquerda, direita;
+
+    public No(String valor){
+        this.valor = valor;
+        esquerda = direita = null;
+    }
+
+
+}
