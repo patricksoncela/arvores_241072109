0c9a3d976c4d32a19011a86db6b384dc7253c3d0
Patrick Soncela <patricksoncela@gmail.com>
2025-05-20 19:58:50 -0300

    contar nós em pré ordem

--- projetoArvore/src/com/company/Arvore.java
+++ projetoArvore/src/com/company/Arvore.java
@@ -9,6 +9,21 @@ public class Arvore {
         }
         return 1 + contarNos(node.esquerda) + contarNos(node.direita);
     }
+
+    public void percorrerPreOrdem(No node){
+        if (node == null) {
+            System.out.println("Árvore vazia");
+        } else {
+            System.out.println(node.valor + " ");
+            System.out.println(node.esquerda);
+            System.out.println(node.direita);
+        }
+    }
+
+    public void percorrerEmOrdem(No node){
+
+    }
+
 }
 
 
