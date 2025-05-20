48724aacbd7c79aa5c1f894bbdbc73d3d10f0fa4
Patrick <133254356+patricksoncela@users.noreply.github.com>
2025-05-20 19:44:32 -0300

    contar nós

--- projetoArvore/src/com/company/Arvore.java
+++ projetoArvore/src/com/company/Arvore.java
@@ -2,4 +2,13 @@ package com.company;
 
 public class Arvore {
     No raiz;
+
+    public int contarNos(No node){
+        if (node == null) {
+            return 0;
+        }
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
 }
+
+
